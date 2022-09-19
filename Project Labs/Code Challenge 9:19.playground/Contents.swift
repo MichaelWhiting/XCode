cf3fffb200ef27e059f9cd92ab9f468f857caeec
Michael Whiting <michaelwhiting@AirPods-Pro.local>
2022-09-19 09:21:02 -0600

    Finished lab 9/19

--- Project Labs/Code Challenge 9:19.playground/Contents.swift
+++ Project Labs/Code Challenge 9:19.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+func calcCentury(year: Int) -> Int {
+    let hasRemainder = year % 100
+    return hasRemainder > 0 ? year / 100 + 1 : year / 100
+}
+
+print(calcCentury(year: 1601))
+print(calcCentury(year: 2000))
+print(calcCentury(year: 1705))
+
